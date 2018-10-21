be4cf61e3e80c9f0693952cc28e5208f8a492bcb
anass.jouhri <anass.jouhri@telecom-sudparis.eu>
2018-10-21 06:07:44 -0700

    Putting in place a code to execute sparql on Univeristy example

---
+++ query-relax/README.md
@@ -0,0 +1,5 @@
+# RUN THE APP USING : mvn exec:java -Dexec.mainClass="org.apache.jena.sparql.Education"
+
+### SPARQL
+
+Query using SPARQL, relying on RDF
---
+++ src/queryRelax/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Licensed under the terms of http://www.apache.org/licenses/LICENSE-2.0
+ */
+package queryRelax;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ok");
+	}
+
+}
