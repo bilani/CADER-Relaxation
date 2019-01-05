22081a2ef0f6f8b889dd329591e667bf5d8ff1de
Tahar BOUBEKEUR <tahar.boubekeur@telecom-sudparis.eu>
2019-01-05 01:00:57 +0100

    Amélioration du controller

--- src/main/java/cader/controllers/QueryController.java
+++ src/main/java/cader/controllers/QueryController.java
@@ -5,8 +5,9 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.jena.ontology.OntModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,18 +18,24 @@ import cader.services.SetDatabase;
 public class QueryController {
 	private static String UPLOADED_FOLDER = "/tmp/";
 
-	@PostMapping(value="/processRequest")
-	public void processRequest(HttpServletRequest request, HttpServletResponse response) throws IllegalStateException, IOException{
-		Boolean isQuery = (Boolean) request.getAttribute("choice"); //to determine if the request is about a file or a querry
-		String database = (String) request.getAttribute("database");
+	@PostMapping("/hello")
+	public String process(HttpServletRequest request, HttpServletResponse response) throws IllegalStateException, IOException{
+		System.out.println(request.toString());
+		@SuppressWarnings("deprecation")
+		Boolean isQuery = new Boolean(request.getParameter("isQuery")); //to determine if the request is about a file or a querry
+		System.out.println("isQuery : " + isQuery);
+		String database = (String) request.getParameter("database");
+		System.out.println("Database choosed : " + database);
 		OntModel model = (OntModel) new SetDatabase(database);
 		if(isQuery) { //it's a query
-			String query = (String) request.getAttribute("query");
+			String query = (String) request.getParameter("query");
+			System.out.println("Received a query : " + query);
 			QueryRelaxer relaxer = new QueryRelaxer(query, model);
+			@SuppressWarnings("unused")
 			File processingResult = new File(relaxer.getFullResults());
-			
+			return "hello";
 		} else { //it's a file
-			
+			return "fuck off";
 		}
 	}
 }
--- src/main/java/cader/webapp/CaderApplication.java
+++ src/main/java/cader/webapp/CaderApplication.java
@@ -6,14 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CaderApplication {
 	/**
-	public static final String DEFAULTQUERY = "SELECT * WHERE { ?X <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://swat.cse.lehigh.edu/onto/univ-bench.owl#Professor> . ?X <http://swat.cse.lehigh.edu/onto/univ-bench.owl#doctoralDegreeFrom> ?Y1 }"; 
-	
-	public static void hello(String[] args) {
-		OntModel model = (new SetDatabase("LUBM10K")).getModel();
-		new QueryRelaxer(DEFAULTQUERY, model);
-	}
-	*/
-	
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CaderApplication.class, args);
 	}
